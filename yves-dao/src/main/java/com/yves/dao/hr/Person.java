75495048e4941bde0364c223d3e514273c1765a6
yijinjin <yijin>
2018-05-09 19:14:43 +0800

    github 测试

--- yves-dao/src/main/java/com/yves/dao/hr/Person.java
+++ yves-dao/src/main/java/com/yves/dao/hr/Person.java
@@ -5,4 +5,14 @@ package com.yves.dao.hr;
  * @date Created in 9:33 2018/5/9
  */
 public class Person {
+    /**
+     * 主键ID
+     */
+    private String id;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
 }
